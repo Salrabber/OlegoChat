4b80f62270fc408a23b2e1265de0d57f2b7c5fbb
Salrabber <salrabber@gmail.com>
2022-08-29 17:29:30 +0300

    first view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
-import './App.scss';
+import React, { Component } from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import Login from "./components/Login";
+import Chat from "./components/Chat";
 
 function App() {
+  const user = false;
+
   return (
-    <div className='container'>
-      <h1>Fuck me</h1>
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          {user ? (
+            <>
+              <Link className="btn btn-success" to="/chat">
+                Open chat
+              </Link>
+              <button type="button" className="btn btn-danger">
+                Log out
+              </button>
+            </>
+          ) : (
+            <Link className="btn btn-primary" to="/login">
+              Login
+            </Link>
+          )}
+        </div>
+      </nav>
+      <div className="container">
+        <div className="wrapper">
+          <h1>Welcome to App-chat</h1>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/chat" element={<Chat />} />
+          </Routes>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AppRouter from "./AppRouter";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+      </div>
+    </nav>
+  );
+}
